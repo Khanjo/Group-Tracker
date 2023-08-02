d42b1e4d76b53020dff3bb5ad0c673c3e59fa136
Joshua Khan <Khanjo@gmail.com>
2023-08-02 10:37:26 -0700

    initialize project 2

---
+++ GroupTracker/Models/Campaign.cs
@@ -0,0 +1,10 @@
+namespace GroupTracker.Models
+{
+    public class Campaign
+    {
+        public string Title { get; set; }
+        public string World { get; set; }
+        public string Summary { get; set; }
+        public int CampaignId { get; set; }
+    }
+}
---
+++ GroupTracker/Models/Character.cs
@@ -0,0 +1,10 @@
+namespace GroupTracker.Models
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Class { get; set; }
+        public int CharacterId { get; set; }
+    }
+}
